b0eb667fce6049a81d87784e825ee80daa68c1a5
Benyamin Galeano <benyamin.galeano@pm.me>
2020-03-17 19:56:16 -0600

    colocando credenciales correctas

--- app/config/mysql.js
+++ app/config/mysql.js
@@ -1,15 +1,9 @@
 const mysql = require('mysql');
-/* const conn = mysql.createConnection({
+const conn = mysql.createConnection({
     host:'18.217.14.101',
     user:'awa_user',
     password: 'Abc12345',
     database: 'awa'
-}); */
-const conn = mysql.createConnection({
-    host:'localhost',
-    user:'phpmyadmin',
-    password: 'galeano4014',
-    database: 'AWA'
 });
 
 conn.connect(err => {
